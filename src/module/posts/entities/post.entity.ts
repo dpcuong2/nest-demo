01f1a073dd251ff4cd4bcc288ce8b7dc6d5fc8a1
dpcuong2 <dinhphucuong2@gmail.com>
2022-11-08 22:58:58 +0700

    make graphql playground

--- src/module/posts/entities/post.entity.ts
+++ src/module/posts/entities/post.entity.ts
@@ -1,7 +1,16 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { User } from 'src/module/users/entities/user.entities';
 
 @ObjectType()
 export class Post {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field(() => String, { description: 'title of the post' })
+  title: string;
+  @Field(() => String, { description: 'body of the post' })
+  body: string;
+  @Field(() => String, { description: 'create timestamp of the post' })
+  createdAt: string;
+  @Field(() => String, { description: 'last updated timestamp of the post' })
+  updatedAt: string;
+  @Field(() => User, { description: 'user of the post' })
+  user: User;
 }
--- src/module/users/entities/user.entities.ts
+++ src/module/users/entities/user.entities.ts
@@ -1,4 +1,5 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { Post } from 'src/module/posts/entities/post.entity';
 
 @ObjectType()
 export class User {
@@ -12,10 +13,15 @@ export class User {
   password: string;
   @Field(() => String, { description: 'gender of the user' })
   gender: Gender;
+  @Field(() => String, { description: 'created time stamp of the user' })
+  createdAt: string;
+  @Field(() => String, { description: 'updated time of the user' })
+  updatedAt: string;
+  @Field(() => [Post], { description: 'list post created by user' })
+  Posts: Post[];
 }
 
-
 export enum Gender {
-    MALE = 'male',
-    FEMALE = 'female',
-  }
+  MALE = 'male',
+  FEMALE = 'female',
+}
